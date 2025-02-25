8c4bc5f516ac95f22dcd75533dfb04524ce19e2c
edbert <edbert.muis@gmail.com>
2025-02-25 19:55:20 +0700

    update middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,13 +2,13 @@ const { verifyToken } = require('../config/auth');
 const authMiddleware = (req, res, next) => {
 
     const token = req.header('Authorization')?.replace('Bearer ', '');
-    if (!token) return res.status(401).json({ message: 'Access denied' });
+    if (!token) return res.status(401).json({ message: 'Akses Ditolak' });
     try {
         const decoded = verifyToken(token);
         req.user = decoded;
         next();
     } catch (err) {
-        res.status(400).json({ message: 'Invalid token' });
+        res.status(400).json({ message: 'Token Tidak Valid' });
     }
 };
 
@@ -16,7 +16,7 @@ const validateTask = (req, res, next) => {
     const { title, category, deadline, status } = req.body;
 
     if (!title || !category || !deadline || !status) {
-        return res.status(400).json({ message: "Semua field harus diisi!" });
+        return res.status(400).json({ message: "Wajib Diisi!" });
     }
 
     next();
